c4f9f0f04355871ac2fe093dfc6d96b658261eff
krishpranav <krisna.pranav@gmail.com>
2021-05-01 09:22:57 +0530

    doc file updated

---
+++ pyvpn/__doc__.py
@@ -0,0 +1,10 @@
+__title__       = "pyvpn"
+__version__     = "0.2.5"
+__license__     = "MIT"
+__description__ = "Python VPN server."
+__keywords__    = "vpn ike esp ip tcp udp wireguard"
+__author__      = "Krish Pranav"
+__email__       = "krisna.pranav@gmail.com"
+__url__         = "https://github.com/krishpranav/pyvpn"
+
+__all__ = ['__title__', '__version__', '__description__', '__url__']
